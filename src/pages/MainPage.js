95698451aae5b84bed7acd7b096b8c9ebaa8c0a4
Jabreu96 <ajoelcrist@gmail.com>
2022-08-21 23:55:56 -0400

    created Main Page and create the Drag and Drop container component file

---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DragnDropContainer from '../components/DragnDropContainer.js'
+
+const MainPage = () => {
+
+
+  return (
+    <>
+      <DragnDropContainer/>
+    </>
+  )
+}
+
+export default MainPage;
