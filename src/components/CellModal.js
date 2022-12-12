d4ae742761a4772e06d17d870d66d3e9555dae10
Jabreu96 <ajoelcrist@gmail.com>
2022-12-12 11:34:26 -0500

    styled card modal to look presentable

--- src/components/CellModal.js
+++ src/components/CellModal.js
@@ -20,92 +20,96 @@ const CellModal = ({ open, toggleModal, cardInfo, cards, changeCardInfo }) => {
     <div>
       <Modal className="modal" open={open} onClose={handleClose}>
         <Box className="modal_card">
-          <form>
-            <p>Date Created</p>
-            <p>{card.dateCreated.toString()}</p>
-            <label>Problem Name</label>
-            <input type="text" defaultValue={card.problemName} onChange={(e) => {
-              changeCardInfo(cardInfo, 'problemName', e.target.value);
-            }}/>
-            <label>Platform</label>
-            <input type="text" defaultValue={card.platform} onChange={(e) => {
-              changeCardInfo(cardInfo, 'platform', e.target.value);
-            }}/>
-            <label>Completed?</label>
-            <input type="radio" defaultValue={'Completed'} defaultChecked={card.completed} onChange={(e) => {
-              changeCardInfo(cardInfo, 'completed', e.target.checked);
-            }}/>
-            <fieldset id='difficulty' >
-              <p>Difficulty</p>
-              <label>Easy</label>
-              <input type="radio" defaultValue={'easy'} name={'difficulty'} defaultChecked={card.difficulty==='easy'}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    changeCardInfo(cardInfo, 'difficulty', 'easy');
-                  }
-                }}/>
-              <label>Medium</label>
-              <input type="radio" defaultValue={'medium'} name={'difficulty'} defaultChecked={card.difficulty === 'medium'}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    changeCardInfo(cardInfo, 'difficulty', 'medium');
-                  }
+          <form className='modal_card_form'>
+            <div className='modal_card_form_left'>
+              <p className='modal_card_form_date'>{card.dateCreated.toString()}</p>
+              <label className='modal_card_form_name'>Problem Name</label>
+              <input className='modal_card_form_name-input' type="text" defaultValue={card.problemName} onChange={(e) => {
+                changeCardInfo(cardInfo, 'problemName', e.target.value);
               }}/>
-              <label>Hard</label>
-              <input type="radio" defaultValue={'hard'} name={'difficulty'} defaultChecked={card.difficulty === 'hard'}
+              <label className='modal_card_form_platform'>Platform</label>
+              <input className='modal_card_form_platform-input' type="text" defaultValue={card.platform} onChange={(e) => {
+                changeCardInfo(cardInfo, 'platform', e.target.value);
+              }}/>
+
+              <label className='modal_card_form_description'>Description</label>
+              <input className='modal_card_form_description-input' type="text" defaultValue={card.description} onChange={(e) => {
+                changeCardInfo(cardInfo, 'description', e.target.value);
+              }}/>
+              <label className='modal_card_form_number'>Problem Number</label>
+              <input className='modal_card_form_number-input' type="text" defaultValue={card.problemNumber} onChange={(e) => {
+                changeCardInfo(cardInfo, 'problemNumber', e.target.value);
+              }}/>
+              {/* THIS IS GOING TO BE A DROPDOWN */}
+              <label className='modal_card_form_datastructure'>Data Structure Type</label>
+              <input className='modal_card_form_datastructure-input' type="text" defaultValue={card.dataStructure}
                 onChange={(e) => {
-                  if (e.target.checked) {
-                    changeCardInfo(cardInfo, 'difficulty', 'hard');
-                  }
+
+                  changeCardInfo(cardInfo, 'dataStructure', e.target.value);
+
                 }}
               />
-            </fieldset>
-            <label>Looked at Solution?</label>
-            <input type="radio" defaultValue={'Looked Up Solution'} defaultChecked={card.solutionLookup} 
-            onChange={(e) => {
-              changeCardInfo(cardInfo, 'solutionLookup', e.target.checked);
-            }}/>
-            <label>Description</label>
-            <input type="text" defaultValue={card.description} onChange={(e) => {
-              changeCardInfo(cardInfo, 'description', e.target.value);
-            }}/>
-            <label>Problem Number</label>
-            <input type="text" defaultValue={card.problemNumber} onChange={(e) => {
-              changeCardInfo(cardInfo, 'problemNumber', e.target.value);
-            }}/>
-            {/* THIS IS GOING TO BE A DROPDOWN */}
-            <label>Data Structure Type</label>
-            <input type="text" defaultValue={card.dataStructure}
+              {/* ----------------------------- */}
+              <label className='modal_card_form_input'>Inputs</label>
+              <input className='modal_card_form_input-input' type="text" defaultValue={card.inputs} 
               onChange={(e) => {
-                
-                changeCardInfo(cardInfo, 'dataStructure', e.target.value);
-                
-              }}
-            />
-            {/* ----------------------------- */}
-            <label>Inputs</label>
-            <input type="text" defaultValue={card.inputs} 
-            onChange={(e) => {
-              changeCardInfo(cardInfo, 'inputs', e.target.value);
-            }}/>
-            <label>Expected Output</label>
-            <input type="text" defaultValue={card.expectedOutput}
-            onChange={(e) => {
-              changeCardInfo(cardInfo, 'expectedOutput', e.target.value);
-            }}/>
-            <label>Constraints</label>
-            <input type="text" defaultValue={card.constraints}
-            onChange={(e) => {
-              changeCardInfo(cardInfo, 'constraints', e.target.value);
-            }}/>
-            <label>Comments</label>
-            <input type="text" defaultValue={card.comments} 
-            onChange={(e) => {
-              changeCardInfo(cardInfo, 'comments', e.target.value);
-            }}/>
-            <Button className="modal_card_btn" size={'large'} color={"secondary"} onClick={() => {
-              toggleModal(-1, '');
-            }}>Save Card</Button>
+                changeCardInfo(cardInfo, 'inputs', e.target.value);
+              }}/>
+              <label className='modal_card_form_output'>Expected Output</label>
+              <input className='modal_card_form_output-input' type="text" defaultValue={card.expectedOutput}
+              onChange={(e) => {
+                changeCardInfo(cardInfo, 'expectedOutput', e.target.value);
+              }}/>
+              <label className='modal_card_form_constraints'>Constraints</label>
+              <input className='modal_card_form_constraints-input' type="text" defaultValue={card.constraints}
+              onChange={(e) => {
+                changeCardInfo(cardInfo, 'constraints', e.target.value);
+              }}/>
+              <label className='modal_card_form_comments'>Comments</label>
+              <input className='modal_card_form_comments-input' type="text" defaultValue={card.comments} 
+              onChange={(e) => {
+                changeCardInfo(cardInfo, 'comments', e.target.value);
+              }}/>
+              <Button className="modal_card_form_btn" size={'large'} color={"secondary"} onClick={() => {
+                toggleModal(-1, '');
+              }}>Save</Button>
+            </div>
+            <div className='modal_card_form_right'>
+              <label className='modal_card_form_completed' class>Completed?</label>
+              <input className='modal_card_form_completed-radio' type="radio" defaultValue={'Completed'} defaultChecked={card.completed} onChange={(e) => {
+                changeCardInfo(cardInfo, 'completed', e.target.checked);
+              }}/>
+              <label className='modal_card_form_lookup' >Looked at Solution?</label>
+              <input className='modal_card_form_lookup-radio' type="radio" defaultValue={'Looked Up Solution'} defaultChecked={card.solutionLookup} 
+              onChange={(e) => {
+                changeCardInfo(cardInfo, 'solutionLookup', e.target.checked);
+              }}/>
+              <fieldset className='modal_card_form_difficulty' id='difficulty' >
+                <p className='modal_card_form_difficulty_title'>Difficulty</p>
+                <label className='modal_card_form_difficulty_easy'>Easy</label>
+                <input className='modal_card_form_difficulty_easy-radio' type="radio" defaultValue={'easy'} name={'difficulty'} defaultChecked={card.difficulty==='easy'}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      changeCardInfo(cardInfo, 'difficulty', 'easy');
+                    }
+                  }}/>
+                <label className='modal_card_form_difficulty_medium' >Medium</label>
+                <input className='modal_card_form_difficulty_medium-radio' type="radio" defaultValue={'medium'} name={'difficulty'} defaultChecked={card.difficulty === 'medium'}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      changeCardInfo(cardInfo, 'difficulty', 'medium');
+                    }
+                }}/>
+                <label className='modal_card_form_difficulty_hard'>Hard</label>
+                <input className='modal_card_form_difficulty_hard-radio' type="radio" defaultValue={'hard'} name={'difficulty'} defaultChecked={card.difficulty === 'hard'}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      changeCardInfo(cardInfo, 'difficulty', 'hard');
+                    }
+                  }}
+                />
+              </fieldset>
+            </div>
           </form>
         </Box>
       </Modal>
